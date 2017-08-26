b0a484863ba1f7921e3c1c1440af2654dd984788
Aníbal A. Lovaglio <alelovaglio@gmail.com>
2017-08-26 19:12:32 -0300

    Fork POC

---
+++ __poc/fork.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import os, sys
+
+from timeout import ProcessTimeout
+
+print("I'm going to fork now - the child will write something to a pipe, and the parent will read it back")
+
+r, w = os.pipe() # these are file descriptors, not file objects
+
+pid = os.fork()
+
+if pid:
+    # we are the parent
+    os.close(w) # use os.close() to close a file descriptor
+    r = os.fdopen(r) # turn r into a file object
+    print("parent: reading")
+
+    process_timer = ProcessTimeout(pid, 10)
+    process_timer.start_timer()
+    print("Process timeout timer launched")
+
+
+    #if the result has been obtained, the is no point on keeping the timer alive
+    if process_timer.ran:
+        print("Execution has been terminated for exceding the timeout limit.")
+    else:
+        process_timer.cancel_timer()
+        print("Process finished correctly without exceding timeout limit.")
+
+    print("stdout captured.")
+
+    txt = r.read()
+    os.waitpid(pid, 0) # make sure the child process gets cleaned up
+else:
+    # we are the child
+    os.close(r)
+    w = os.fdopen(w, 'w')
+    print("child: writing")
+    
+
+    import os, shutil, subprocess
+
+    # move to the jail
+    os.chroot('/var/chroot')
+
+    current_dir = os.getcwd()
+    script_dir = '/tmp/kwyjibo'
+    script = '/opt/kwyjibo/script.sh'
+    # os.chdir(script_dir)
+
+    process = subprocess.Popen([script], shell=True, stdout = subprocess.PIPE, stderr=subprocess.PIPE)
+
+    exit_value = process.wait()
+    output = process.communicate()
+    captured_stdout = output[0]
+    print(captured_stdout)
+    sys.exit(exit_value)
+
+
+print("parent: got it; text =", txt)
--- __poc/timeout.py
+++ __poc/timeout.py
@@ -8,29 +8,29 @@ import os, psutil, signal
 
 class ProcessTimeout:
     """
-    Handler of the timer for setting a timeout to the automatic correction process
+    Handler of the timer for setting a timeout to the automatic correction pid
     """
 
 
-    def __init__(self, process, timeout):
-        self.process = process
+    def __init__(self, pid, timeout):
+        self.pid = pid
         self.timeout = timeout
         self.ran = False
         self.timer = Timer(self.timeout, self.kill_proc)
 
     def killtree(self):
-        parent = psutil.Process(self.process.pid)
+        parent = psutil.Process(self.pid)
         for child in parent.get_children(recursive=True):
-            print("killing process %d", child.pid)
+            print("killing pid %d", child.pid)
             # child.kill()
             os.kill(child.pid, signal.SIGKILL)
-        print("killing process %d", parent.pid)
+        print("killing pid %d", parent.pid)
         parent.kill()
 
     def kill_proc(self):
-        print("timer expired, killing process...")
+        print("timer expired, killing pid...")
         self.killtree()
-        print("process terminate invoked.")
+        print("pid terminate invoked.")
         self.ran = True
 
     def start_timer(self):
