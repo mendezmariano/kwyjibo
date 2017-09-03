397be09b4e06d5ddec22d20a1c6fe399d8326c09
Aníbal A. Lovaglio <alelovaglio@gmail.com>
2017-09-03 17:54:22 -0300

    wsgi path

--- kwyjibo/wsgi.py
+++ kwyjibo/wsgi.py
@@ -7,6 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+import sys
+print(sys.path)
+
 import os
 
 from django.core.wsgi import get_wsgi_application
