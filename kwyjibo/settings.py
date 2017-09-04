fdfd711bab1fa56493c17a6deb8224f2504a402e
Aníbal A. Lovaglio <alelovaglio@gmail.com>
2017-09-03 23:21:00 -0300

    path fix

--- kwyjibo/settings.py
+++ kwyjibo/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '' 
 
-STATIC_ROOT = os.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
