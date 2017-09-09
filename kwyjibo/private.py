febf732fa59cf32de154486cfa885cab0afb78b5
Aníbal A. Lovaglio <alelovaglio@gmail.com>
2017-09-09 12:56:18 -0300

    Revision automation... still cannot get this right...

--- kwyjibo/private.py
+++ kwyjibo/private.py
@@ -3,3 +3,6 @@ SECRET_KEY = '7!o9x^aj=-y4*c3145f&l40@-r%yg8=i($&*p@atzw7dd@f=1u'
 
 DB_USER = 'kwyjibo'
 DB_PASSWORD = 'kwyjibo321'
+
+# Revisions
+POST_END_POINT = 'localhost'
--- revisions/local_settings.py
+++ revisions/local_settings.py
@@ -1,3 +1,5 @@
+from kwyjibo.settings import *
+
 
 REVISION_BATCH_SIZE = 10
 REVISION_TIMEOUT = 30 # seconds
@@ -5,7 +7,6 @@ REVISION_TIMEOUT = 30 # seconds
 JAIL_ROOT = '/var/chroot'
 EXECUTION_ROOT = '/tmp/kwyjibo'
 
-POST_END_POINT = 'localhost'
-POST_REVISION_URL = 'http://{endpoint}/revisions/revision'.format(endpoint = POST_END_POINT)
-POST_MAIL_URL = 'http://{endpoint}/revisions/mail'.format(endpoint = POST_END_POINT)
+POST_REVISION_URL = 'http://{endpoint}/revisions/revision/'.format(endpoint = POST_END_POINT)
+POST_MAIL_URL = 'http://{endpoint}/revisions/mail/'.format(endpoint = POST_END_POINT)
 
