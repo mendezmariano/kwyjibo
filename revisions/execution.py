d9e7a156d26db6d7bdc9d3f9b2ca219342121667
Anibal A. Lovaglio <alelovaglio@gmail.com>
2018-05-05 23:18:02 -0300

    Add cleanup replacement for resutl publishing

--- revisions/execution.py
+++ revisions/execution.py
@@ -81,7 +81,7 @@ class SafeCodeRunner(object):
         accumulated = ''
         txt = str(r.readline())
         while txt and len(accumulated) <= REVISION_OUTPUT_MAX_LENGTH:
-            accumulated = accumulated + txt.replace("\n", "\\n").replace('"', r'\"') # FIXME: This should be way more elegant
+            accumulated = accumulated + txt.replace("\r", "").replace("\n", r'\n').replace('"', r'\"') # FIXME: This should be way more elegant
             txt = r.readline()
 
         if len(accumulated) > REVISION_OUTPUT_MAX_LENGTH:
